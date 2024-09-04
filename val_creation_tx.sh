ed6e41dd138a3c3cd5140f0a95ffe3553e885ee1
KrakeNocto <octo2kraken@gmail.com>
2024-09-04 10:54:09 +0300

    Update val_creation_tx.sh

--- val_creation_tx.sh
+++ val_creation_tx.sh
@@ -4,14 +4,14 @@ min_sum=600000000000000000
 max_sum=900000000000000000
 random_sum=$(shuf -i $min_sum-$max_sum -n 1)
 
-min_time_s=480
-max_time_s=720
-random_time=$(shuf -i $min_time_s-$max_time_s -n 1)
 
 echo "Enter private key:"
 read -r PK
 
-echo "Staking amount: $random_sum, Create tx after $random_time seconds"
+echo "Enter time:"
+read -r time
 
-sleep $random_time
+echo "Staking amount: $random_sum, Create tx after $time seconds"
+
+sleep $time
 story validator create --stake $random_sum --private-key="$PK"
