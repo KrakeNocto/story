563565708ee31738421e8042c36cef2c815f2431
KrakeNocto <octo2kraken@gmail.com>
2024-09-17 20:48:04 +0300

    Update story_upd.sh

--- story_upd.sh
+++ story_upd.sh
@@ -6,5 +6,7 @@ wget http://116.202.116.35:33150/story
 rm /usr/local/bin/story
 mv story /usr/local/bin/
 
+chmod +x /usr/local/bin/story
+
 systemctl restart story-testnet-geth story-testnet
 journalctl -fu story-testnet
