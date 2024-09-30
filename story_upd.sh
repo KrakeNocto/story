f938e49324dbd4f6c13febb5c8bcaa7e0b5cc349
KrakeNocto <octo2kraken@gmail.com>
2024-09-30 11:55:46 +0300

    Update story_upd.sh

--- story_upd.sh
+++ story_upd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm story_upd*
+
 systemctl stop story-testnet
 
 wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/story-public/story-linux-amd64-0.10.1-57567e5.tar.gz
@@ -9,6 +11,8 @@ tar -xzf story-linux-amd64-0.10.1-57567e5.tar.gz
 mv story-linux-amd64-0.10.1-57567e5/story /usr/local/bin/
 chmod +x /usr/local/bin/story
 rm -rf story-linux-amd64-0.10.1-57567e5*
+
+rm story_upd*
 
 systemctl restart story-testnet-geth story-testnet
 journalctl -fu story-testnet
