344379b3c234cc6040b1a76636ab700ddfd3cb2d
KrakeNocto <octo2kraken@gmail.com>
2024-10-17 11:58:54 +0300

    Update story_upd.sh

--- story_upd.sh
+++ story_upd.sh
@@ -4,13 +4,20 @@ rm story_upd*
 
 systemctl stop story-testnet
 
-wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/story-public/story-linux-amd64-0.10.1-57567e5.tar.gz
+wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/story-public/story-linux-amd64-0.11.0-aac4bfe.tar.gz
+wget https://github.com/piplabs/story-geth/releases/download/v0.9.4/geth-linux-amd64
+
+tar -xzf story-linux-amd64-0.11.0-aac4bfe.tar.gz
+mv story-linux-amd64-0.11.0-aac4bfe/story /usr/local/bin/
+mv geth-linux-amd64 version /usr/local/bin/geth
+
 chmod +x /usr/local/bin/story
-rm /usr/local/bin/story
-tar -xzf story-linux-amd64-0.10.1-57567e5.tar.gz
-mv story-linux-amd64-0.10.1-57567e5/story /usr/local/bin/
-chmod +x /usr/local/bin/story
-rm -rf story-linux-amd64-0.10.1-57567e5*
+chmod +x /usr/local/bin/geth
+
+rm story-linux-amd64-0.11.0-aac4bfe.tar.gz
+rm -rf story-linux-amd64-0.11.0-aac4bfe
+
+wget -O $HOME/.story/story/config/addrbook.json https://server-3.itrocket.net/testnet/story/addrbook.json
 
 rm story_upd*
 
