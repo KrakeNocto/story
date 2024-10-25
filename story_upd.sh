869e5b4a68135b9ebf1101d49ae9c7adf892b72c
KrakeNocto <octo2kraken@gmail.com>
2024-10-25 16:32:40 +0300

    Update story_upd.sh

--- story_upd.sh
+++ story_upd.sh
@@ -4,28 +4,14 @@ rm story_upd*
 
 systemctl stop story-testnet story-testnet-geth
 
-wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/story-public/story-linux-amd64-0.11.0-aac4bfe.tar.gz
-wget https://github.com/piplabs/story-geth/releases/download/v0.9.4/geth-linux-amd64
+wget https://github.com/piplabs/story/releases/download/v0.12.0/story-linux-amd64
+wget https://github.com/piplabs/story-geth/releases/download/v0.10.0/geth-linux-amd64
 
-tar -xzf story-linux-amd64-0.11.0-aac4bfe.tar.gz
-mv story-linux-amd64-0.11.0-aac4bfe/story /usr/local/bin/
+mv story-linux-amd64 /usr/local/bin/
 mv geth-linux-amd64 /usr/local/bin/geth
 
 chmod +x /usr/local/bin/story
 chmod +x /usr/local/bin/geth
-
-rm story-linux-amd64-0.11.0-aac4bfe.tar.gz
-rm -rf story-linux-amd64-0.11.0-aac4bfe
-
-wget -O $HOME/.story/story/config/addrbook.json https://server-3.itrocket.net/testnet/story/addrbook.json
-
-cp $HOME/.story/story/data/priv_validator_state.json $HOME/.story/story/priv_validator_state.json.backup
-rm -rf $HOME/.story/story/data
-rm -rf $HOME/.story/geth/iliad/geth/chaindata
-wget -O story_testnet_1620492.tar.lz4 https://support.synergynodes.com/snapshots/story_testnet_story/story_testnet_1532088.tar.lz4
-lz4 -c -d story_testnet_geth_1620492.tar.lz4 | tar -x -C $HOME/.story/story
-rm story_testnet_1620492.tar.lz4
-mv $HOME/.story/story/priv_validator_state.json.backup $HOME/.story/story/data/priv_validator_state.json
 
 rm story_upd*
 
